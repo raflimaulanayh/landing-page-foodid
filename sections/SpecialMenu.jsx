fd930f367e66206b56e4489761b713d401965cc0
RafliSky <raflimaulana214@gmail.com>
2023-02-25 22:43:35 +0700

    mapping special menu, masih ada bug pas bagian swippernya

--- sections/SpecialMenu.jsx
+++ sections/SpecialMenu.jsx
@@ -1,8 +1,5 @@
 import React from "react";
-import { Pagination } from "swiper";
-import { Swiper, SwiperSlide } from "swiper/react";
-import "swiper/css";
-import "swiper/css/pagination";
+
 import CardsSpecial from "@/components/CardsSpecial";
 import TitleSection from "@/components/TitleSection";
 
@@ -13,66 +10,10 @@ const SpecialMenu = () => {
         <TitleSection title="SPECIAL MENU" />
       </div>
       <div className="xl:hidden flex flex-wrap justify-center">
-        <Swiper
-          modules={[Pagination]}
-          spaceBetween={20}
-          slidesPerView={1}
-          pagination={{ clickable: true }}
-          onSwiper={(swiper) => console.log(swiper)}
-          onSlideChange={() => console.log("slide change")}
-        >
-          <SwiperSlide>
-            <CardsSpecial
-              imgSpecialMenu="/assets/tutug-oncom.png"
-              titleSpecialMenu="Nasi Tutug Oncom"
-              imgRating="/assets/star-rating.svg"
-              hargaSpecialMenu="12.500"
-            />
-          </SwiperSlide>
-          <SwiperSlide>
-            <CardsSpecial
-              imgSpecialMenu="/assets/karedok.png"
-              titleSpecialMenu="Karedok"
-              imgRating="/assets/star-rating.svg"
-              hargaSpecialMenu="10.000"
-            />
-          </SwiperSlide>
-          <SwiperSlide>
-            <CardsSpecial
-              imgSpecialMenu="/assets/sambel-pencok.png"
-              titleSpecialMenu="Sambel Pencok"
-              imgRating="/assets/star-rating.svg"
-              hargaSpecialMenu="5.000"
-            />
-          </SwiperSlide>
-          <SwiperSlide>
-            <CardsSpecial
-              imgSpecialMenu="/assets/nasi-liwet.png"
-              titleSpecialMenu="Nasi Pindang"
-              imgRating="/assets/star-rating.svg"
-              hargaSpecialMenu="37.000"
-            />
-          </SwiperSlide>
-          <SwiperSlide>
-            <CardsSpecial
-              imgSpecialMenu="/assets/gurame-bakar.png"
-              titleSpecialMenu="Gurame Bakar"
-              imgRating="/assets/star-rating.svg"
-              hargaSpecialMenu="21.000"
-            />
-          </SwiperSlide>
-          <SwiperSlide>
-            <CardsSpecial
-              imgSpecialMenu="/assets/tahu-sumedang.png"
-              titleSpecialMenu="Tahu Sumedang"
-              imgRating="/assets/star-rating.svg"
-              hargaSpecialMenu="15.000"
-            />
-          </SwiperSlide>
-        </Swiper>
+        <CardsSpecial />
       </div>
       <div className="hidden xl:flex flex-wrap justify-center">
-        <Swiper
+        {/* <Swiper
           modules={[Pagination]}
           spaceBetween={30}
           slidesPerView={3}
@@ -81,54 +22,9 @@ const SpecialMenu = () => {
           onSlideChange={() => console.log("slide change")}
         >
           <SwiperSlide>
-            <CardsSpecial
-              imgSpecialMenu="/assets/tutug-oncom.png"
-              titleSpecialMenu="Nasi Tutug Oncom"
-              imgRating="/assets/star-rating.svg"
-              hargaSpecialMenu="12.500"
-            />
+            <CardsSpecial />
           </SwiperSlide>
-          <SwiperSlide>
-            <CardsSpecial
-              imgSpecialMenu="/assets/karedok.png"
-              titleSpecialMenu="Karedok"
-              imgRating="/assets/star-rating.svg"
-              hargaSpecialMenu="10.000"
-            />
-          </SwiperSlide>
-          <SwiperSlide>
-            <CardsSpecial
-              imgSpecialMenu="/assets/sambel-pencok.png"
-              titleSpecialMenu="Sambel Pencok"
-              imgRating="/assets/star-rating.svg"
-              hargaSpecialMenu="5.000"
-            />
-          </SwiperSlide>
-          <SwiperSlide>
-            <CardsSpecial
-              imgSpecialMenu="/assets/nasi-liwet.png"
-              titleSpecialMenu="Nasi Pindang"
-              imgRating="/assets/star-rating.svg"
-              hargaSpecialMenu="37.000"
-            />
-          </SwiperSlide>
-          <SwiperSlide>
-            <CardsSpecial
-              imgSpecialMenu="/assets/gurame-bakar.png"
-              titleSpecialMenu="Gurame Bakar"
-              imgRating="/assets/star-rating.svg"
-              hargaSpecialMenu="21.000"
-            />
-          </SwiperSlide>
-          <SwiperSlide>
-            <CardsSpecial
-              imgSpecialMenu="/assets/tahu-sumedang.png"
-              titleSpecialMenu="Tahu Sumedang"
-              imgRating="/assets/star-rating.svg"
-              hargaSpecialMenu="15.000"
-            />
-          </SwiperSlide>
-        </Swiper>
+        </Swiper> */}
       </div>
     </section>
   );
