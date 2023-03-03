b17c8db81b0e891aa57e575492c5d95d8446d3a6
RafliSky <raflimaulana214@gmail.com>
2023-03-03 11:14:16 +0700

    -;-

--- components/CardsChooseFoods.jsx
+++ components/CardsChooseFoods.jsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const ChooseFoods = ({ imgChooseFoods, titleChooseFoods, descChooseFoods }) => {
   return (
-    <div className="flex flex-col justify-center bg-white shadow-lg h-[305px] w-full md:w-[350px] px-10 text-center py-20">
+    <div className="flex flex-col justify-center bg-white shadow-lg rounded-lg h-[305px] w-full md:w-[350px] px-10 text-center py-20">
       <img src={imgChooseFoods} alt="" className="mx-auto" />
       <h2 className="mt-5 font-semibold text-3xl">{titleChooseFoods}</h2>
       <p className="mt-4 text-gray-500">{descChooseFoods}</p>
